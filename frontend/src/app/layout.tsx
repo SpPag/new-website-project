e053cf9eb827bb949a1419af44bc867c1bbbd36d
SpPag <spiridonpagiatis@gmail.com>
2025-09-12 14:27:32 +0300

    style(whole website): change tab title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Nick the Greek",
+  title: "Nick's Guitar Lessons",
   description: "music, lessons, guitar",
 };
 
