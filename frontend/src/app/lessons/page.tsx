8781347507b7c3b3b47e866a2a76e818272fd215
SpPag <spiridonpagiatis@gmail.com>
2025-10-01 13:43:15 +0300

    feat(frontend): add light / dark mode styling, the Contact button and the lessons grid to the lessons page

--- frontend/src/app/lessons/page.tsx
+++ frontend/src/app/lessons/page.tsx
@@ -1,9 +1,11 @@
+import { ContactButton } from "@/components/ContactButton";
 import { HomeButton } from "@/components/HomeButton";
+import { LessonCard } from "@/components/LessonCard";
 
 export default function LessonsPage() {
 
 	return (
-		<div className="h-screen relative text-zinc-800">
+		<div className="h-screen relative dark:text-zinc-200">
 			{/* Background image */}
 			<div
 				className="absolute inset-0 bg-cover bg-center"
@@ -11,18 +13,38 @@ export default function LessonsPage() {
 			/>
 
 			{/* Semi-transparent overlay */}
-			<div className="absolute inset-0 bg-white/30"></div>
+			<div className="absolute inset-0 bg-white/40 dark:bg-black/20"></div>
 
 			{/* Home button */}
-			<div className="z-20 absolute top-2 left-6 mt-4 text-white flex">
+			<div className="z-20 absolute top-2 left-6 mt-4 dark:text-zinc-200 flex">
 				<HomeButton />
 			</div>
 
-			{/* Content */}
-			<div className="relative z-10 flex flex-col items-center pt-8 font-sans">
+			{/* Contact button */}
+			<div className="z-10 absolute top-2 right-6 mt-4 dark:text-zinc-200 flex space-x-2">
+				<ContactButton />
+			</div>
+
+			{/* Title */}
+			<div className="relative flex flex-col items-center pt-8 font-sans">
 				<div className="text-3xl mb-10">
 					Μαθήματα
 				</div>
+			</div>
+
+			{/* Lessons */}
+			<div className="relative z-10 grid h-[calc(100vh-150px)] overflow-y-auto sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 pb-8">
+				{Array.from({ length: 22 }).map((_, index) => {
+					const randomPrice = Math.floor(Math.random() * 51) + 20; // random between 20 and 70
+					return (
+						<LessonCard
+							key={index}
+							title={`Lesson #${index + 1}`}
+							description={`Description for Lesson #${index + 1}`}
+							price={randomPrice}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
