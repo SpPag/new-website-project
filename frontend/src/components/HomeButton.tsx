2d2f43215b78dd0f2ac4f6d893a3a8d4d17fb059
SpPag <spiridonpagiatis@gmail.com>
2025-10-01 11:49:51 +0300

    feat(frontend): add components for Home, Contact and Lessons buttons

---
+++ frontend/src/components/HomeButton.tsx
@@ -0,0 +1,17 @@
+const HomeButton = () => {
+	return (
+		<a
+			href="/"
+			className="
+				px-10 py-2
+				bg-zinc-600 hover:bg-zinc-700 transition 
+				border border-gray-950 rounded-lg
+				shadow
+				"
+		>
+			Αρχική
+		</a>
+	);
+};
+
+export { HomeButton }
---
+++ frontend/src/components/LessonsButton.tsx
@@ -0,0 +1,17 @@
+const LessonsButton = () => {
+	return (
+		<a
+			href="/lessons"
+			className="
+				px-10 py-2
+				bg-zinc-600 hover:bg-zinc-700 transition
+				border border-gray-950 rounded-lg
+				shadow
+				"
+		>
+			Μαθήματα
+		</a>
+	);
+};
+
+export { LessonsButton }
